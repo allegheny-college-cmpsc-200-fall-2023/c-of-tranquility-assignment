15c44df678c370568e3806b395d1d263bca34e1d
dluman <dluman@allegheny.edu>
2023-10-19 23:39:10 -0400

    Remove code from main.c

--- disassembly/main.c
+++ disassembly/main.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
-
 #include "pico/stdlib.h"
 
-int ndown(int count, int increment) {
-    return count - increment;
-}
 
 int main(void) {
     stdio_init_all();
-    int count = 10;
-    while (count > 0){
-        printf("%d\n", count);
-        count = ndown(count, 2);
-    }
-    printf("LIFTOFF!");
-    return 0;
 }
